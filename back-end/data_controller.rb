ac461f3aa2d10c8e2342728f92e003a55fe6d493
Bo Zhang <boofcourse@gmail.com>
2019-10-24 12:07:04 -0700

    added validate

--- back-end/data_controller.rb
+++ back-end/data_controller.rb
@@ -1,11 +1,33 @@
 class DataController < ApplicationController
-  def validate_params
+  TEMPERATURE = 'temperature'.freeze 
+  NOISE = 'noise'.freeze
+  PRESSURE = 'pressure'.freeze
+  HUMIDITY = 'humidity'.freeze
+  LIGHT = 'light'.freeze
+
+  VALID_METRICS = [TEMPERATURE, NOISE, PRESSURE, HUMIDITY, LIGHT].freeze
+
+  def valid_params?(metrics)
+    metrics.each do |metric_key|
+      possible_metric = metrics[metric_key]
+      return false if !VALID_METRICS.include?(possible_metric)
+    end
   end
 
-  def sanitize_params
+  def valid_timestamp?(start_time, stop_time)
+    return true if start_time <= Time.Now && stop_time > Time.Now
+  end
+  
+
+  def sanitize_params(metrics, start_time, stop_time)
+    return true if validate_params?(metrics) && validate_start_time(start_time, stop_time)
   end
 
   def connect_influxdb  
   end
 
 end
+
+
+
+
