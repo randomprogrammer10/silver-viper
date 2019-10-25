533529d4f63dc6c27235b85fa37fbf37762be8c6
James Livermont <j_livermont@yahoo.com>
2019-10-24 18:01:09 -0700

    Added new fields and new /tick endpoint

--- back-end/app.rb
+++ back-end/app.rb
@@ -16,13 +16,23 @@ get '/data' do
   metrics = params[:metrics]
   return 422 unless controller.valid_metrics?(metrics)
 
-  start_time = Time.at(params[:start_time].to_i).to_datetime
-  stop_time = Time.at(params[:stop_time].to_i).to_datetime
+  start_time = Time.at(params[:start_time].to_i)
+  stop_time = Time.at(params[:stop_time].to_i)
   return 422 unless controller.valid_timestamps?(start_time, stop_time)
 
   json controller.fetch_data(start_time, stop_time, metrics)
 rescue StandardError
   return 422
+end
+
+get '/tick' do
+  controller = DataController.new
+
+  # Return values for the last 6 seconds
+  stop_time = Time.now
+  start_time = Time.now - 6
+
+  json controller.fetch_data(start_time, stop_time)
 end
 
 # This is the default route that will be applied if there are no matches above
--- back-end/data_controller.rb
+++ back-end/data_controller.rb
@@ -3,14 +3,25 @@ require 'date'
 require_relative 'influx_client'
 
 class DataController
-  TEMPERATURE = 'temperature'.freeze
-  NOISE = 'noise'.freeze
-  PRESSURE = 'pressure'.freeze
-  HUMIDITY = 'humidity'.freeze
-  LIGHT = 'light'.freeze
-
   VALID_PARAMS = ['start_time', 'stop_time', 'metrics'].freeze
-  VALID_METRICS = [TEMPERATURE, NOISE, PRESSURE, HUMIDITY, LIGHT].freeze
+  VALID_METRICS = %w(
+    temperature
+    pressure
+    humidity
+    light
+    gas_nh3
+    gas_oxidizing
+    gas_reducing
+    pm_per_dl_0_3
+    pm_per_dl_0_5
+    pm_per_dl_10_0
+    pm_per_dl_1_0
+    pm_per_dl_2_5
+    pm_per_dl_5_0
+    pm_ug_per_m3_10
+    pm_ug_per_m3_1_0
+    pm_ug_per_m3_2_5
+  )
 
   def valid_metrics?(metrics)
     return true if metrics.nil?
@@ -20,15 +31,22 @@ class DataController
 
   def valid_timestamps?(start_time, stop_time)
     return false if start_time.nil? || stop_time.nil?
-    return true if start_time < stop_time && stop_time <= DateTime.now
+    return true if start_time < stop_time && stop_time <= Time.now
     return false
   end
 
   def fetch_data(start_time, stop_time, metrics=nil)
     metrics = VALID_METRICS if metrics.nil?
     result = InfluxClient.new.get_metrics_data(metrics, start_time, stop_time)
+    return [] if result.nil? || result.empty?
 
-    # Return only the data values, none of the other metadata
-    return result[0]['values']
+    result = result[0]['values']
+    transform_timestamps(result)
+
+    result
+  end
+
+  def transform_timestamps(result)
+    result.each { |entry| entry['time'] = Time.parse(entry['time']).to_i }
   end
 end
